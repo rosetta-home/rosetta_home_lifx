71b09794d178cdf606b782e3f87949c7a9b69ab9
Christopher Cote <chris@entropealabs.com>
2017-02-21 17:20:20 -0600

    add cicada dependencies

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule RosettaHomeLifx.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :lifx]]
+    [extra_applications: [:logger, :lifx, :network_manager, :device_manager]]
   end
 
   # Dependencies can be Hex packages:
