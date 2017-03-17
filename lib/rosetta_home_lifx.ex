e267b39003dab6cbf04ed596aaff45a6e7ffae64
Christopher Coté <chris@entropealabs.com>
2017-03-17 15:47:38 -0500

    update for histograms

--- lib/rosetta_home_lifx.ex
+++ lib/rosetta_home_lifx.ex
@@ -1,5 +1,5 @@
 defmodule Cicada.DeviceManager.Device.Light.Lifx do
-  use GenServer
+  use Cicada.DeviceManager.DeviceHistogram
   require Logger
   alias Cicada.{DeviceManager, VoiceControl}
   @behaviour Cicada.DeviceManager.Behaviour.Light
